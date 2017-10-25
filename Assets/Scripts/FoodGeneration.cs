9408d782411532b867d060030d342fd5b1fd3eae
Keinael <usenko.ua@gmail.com>
2017-10-25 21:24:57 +0300

    Creating scene, addit food generation

---
+++ Assets/Scripts/FoodGeneration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodGeneration : MonoBehaviour {
+
+	public GameObject foodPrefab;
+
+	public float xSize = 8.9f;
+
+	public float zSize = 8.9f;
+
+	public GameObject curFood;
+
+	public Vector3 curPos;
+
+	void Start () {
+		RandomPos ();
+		curFood = GameObject.Instantiate(foodPrefab,curPos,Quaternion.identity) as GameObject;	
+	}
+
+	void RandomPos()
+	{
+		curPos = new Vector3 (Random.Range(xSize*-1,xSize),0.2f,Random.Range(zSize*-1,zSize));
+	}
+
+	void Update () {
+		
+	}
+}
